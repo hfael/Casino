01072e426f961956403ac78c14c51f334bf0c6fd
fael <48855635+hfael@users.noreply.github.com>
2024-11-03 23:31:38 +0100

    Create Functions.java

---
+++ fr/fael/casino/advanced/Functions.java
@@ -0,0 +1,26 @@
+package fr.fael.casino;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class Functions {
+
+	static Set<Integer> rougeValue = new HashSet<>(Set.of(1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36));
+	
+	public static String getColor(int number) {
+		if(number == 0) return "Vert";
+		if(rougeValue.contains(number)) return "Rouge";
+		return "Noir";
+	}
+	
+	public static int getRandomInt(int maxInt) {
+		Random random = new Random();
+		int number = random.nextInt(maxInt);
+		return number;
+	}
+	
+	public static void log(String string) {
+		System.out.println(string);
+	}
+}
