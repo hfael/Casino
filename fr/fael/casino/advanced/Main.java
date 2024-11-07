aeeaf7c0d092f4ade9d07e6482f8563a1d94c45b
fael <48855635+hfael@users.noreply.github.com>
2024-11-07 22:02:08 +0100

    Update Main.java

--- fr/fael/casino/advanced/Main.java
+++ fr/fael/casino/advanced/Main.java
@@ -1,1 +1,18 @@
-aaaaa
+package fr.fael.casino.advanced;
+
+import fr.fael.casino.nodes.Nodes1;
+
+public class Main {
+	
+	public static double wallet = 5000;
+	public static double mise = 100;
+	public static double benefice = 400; // en %
+	
+	public static double final_benefice = ((wallet * benefice) / 100) + wallet;
+	
+	public static void main(String[] args) throws InterruptedException {
+		//String result = Functions.roulette2();
+		//Functions.log(result);
+		Functions.log("\nTaux de chance d'y arriver: " + Nodes1.N1() + "%");
+	}
+}
