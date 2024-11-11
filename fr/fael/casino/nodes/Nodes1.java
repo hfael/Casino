8ee751133a3ec68b26a3560a087a72ad226f4d66
fael <48855635+hfael@users.noreply.github.com>
2024-11-11 22:47:53 +0100

    Update Nodes1.java

--- fr/fael/casino/nodes/Nodes1.java
+++ fr/fael/casino/nodes/Nodes1.java
@@ -1,23 +1,23 @@
 package fr.fael.casino.nodes;
 
+import fr.fael.casino.advanced.Functions;
 import fr.fael.casino.advanced.Main;
 import fr.fael.casino.advanced.strats.Martingale;
 
 public class Nodes1 {
-
     private static final double baseWallet = Main.wallet;
     private static final double baseMise = Main.mise;
     private static final double baseBenefice = Main.final_benefice;
 
-    public static double N1() {
-        double victory = 0;
-
+    public static int N1() {
+        int victories = 0;
         for (int i = 0; i < 100; i++) {
-            int result = Martingale.martingale(baseWallet, baseMise, baseBenefice);
-            if(result == 1) {
-                victory++;
+        	Functions.log("a");
+            if (Martingale.martingale(baseWallet, baseMise, baseBenefice) == 1) {
+                victories++;
+            	Functions.log("b " + victories);
             }
         }
-        return victory;
+        return victories;
     }
 }
