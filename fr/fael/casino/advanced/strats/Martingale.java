81bf8e8a89c5144c0ffd95ddb31fd134ccbe3426
fael <48855635+hfael@users.noreply.github.com>
2024-11-11 22:48:46 +0100

    Update Martingale.java

--- fr/fael/casino/advanced/strats/Martingale.java
+++ fr/fael/casino/advanced/strats/Martingale.java
@@ -5,32 +5,26 @@ import fr.fael.casino.advanced.Main;
 
 public class Martingale {
 
-    private static final double baseWallet = Main.wallet;
     private static final double baseBenefice = Main.final_benefice;
 
     public static int martingale(double wallet, double mise, double benefice) {
-        double walletMax = wallet;
         double actualMise = mise;
 
-
-        while (wallet < baseBenefice) {
+        while (wallet < baseBenefice && wallet > actualMise) {
             wallet -= actualMise;
-            int roulette = Functions.roulette();
+            
+            if (actualMise > Main.mise_max) {
+                return 0;
+            }
 
-
-            if (roulette == 1) {
+            int rouletteResult = Functions.roulette();
+            if (rouletteResult == 1) {
                 wallet += actualMise * 2;
                 actualMise = mise;
             } else {
                 actualMise *= 2;
             }
-
-            walletMax = Math.max(walletMax, wallet);
-
-            if (wallet <= 0) {
-                return 0;
-            }
         }
-        return (wallet > baseWallet) ? 1 : 0;
+        return 0;
     }
 }
