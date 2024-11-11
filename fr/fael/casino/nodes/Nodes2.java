d6e29d0acc331b0e5ff9bd600eecc120d1482b7c
fael <48855635+hfael@users.noreply.github.com>
2024-11-11 22:48:06 +0100

    Update Nodes2.java

--- fr/fael/casino/nodes/Nodes2.java
+++ fr/fael/casino/nodes/Nodes2.java
@@ -4,51 +4,12 @@ import fr.fael.casino.advanced.Main;
 import fr.fael.casino.advanced.strats.Martingale;
 
 public class Nodes2 {
-    
+
     static double wallet = Main.wallet;
     static double benefice = Main.final_benefice;
     
-    public static double N2(double winrate, double initialMise) {
-        double bestMise = 0;
-        double highestSuccessRate = 0;
-        
-        for (double mise = initialMise; mise <= 200; mise += 10) {
-            int successfulRuns = 0;
-            
-            for (int j = 0; j < 100; j++) {
-                double currentMise = mise;
-                double tempWallet = wallet;
-                
-                while (tempWallet > currentMise && tempWallet < benefice) {
-                    int result = Martingale.martingale(tempWallet, currentMise, benefice);
-                    if (result == 1) {
-                        successfulRuns++;
-                        break;
-                    } else {
-                        currentMise *= 2;
-                        if (currentMise > tempWallet) break;
-                    }
-                }
-            }
-            
-            double currentSuccessRate = (double) successfulRuns / 100;
-            
-            if (currentSuccessRate > highestSuccessRate) {
-                highestSuccessRate = currentSuccessRate;
-                bestMise = mise;
-            }
-        }
-        return bestMise;
-    }
-
-    public static double moyenneMeilleureMise(double winrate, int iterations, double initialMise) {
-        double totalBestMise = 0;
-        
-        for (int i = 0; i < iterations; i++) {
-            double bestMiseForRun = N2(winrate, initialMise);
-            totalBestMise += bestMiseForRun;
-        }
-        
-        return totalBestMise / iterations;
+    public static double N2(double initialMise) {
+    	
+    	return 0;
     }
 }
